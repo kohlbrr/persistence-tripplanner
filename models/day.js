567d8826023c719a8e8ca3bbc4fce5c46ccc819e
kohlbrr <kohlbrr@gmail.com>
2017-05-10 17:51:20 -0400

    We killed it \o/

---
+++ models/day.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+var db = require('./_db');
+
+var Day = db.define('day', {
+  number: {
+    type: Sequelize.INTEGER
+  }
+});
+
+module.exports = Day;
